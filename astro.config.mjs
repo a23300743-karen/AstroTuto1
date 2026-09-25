4015f4219d2d89c95eaa23058512576773312355
karen <a23300743@ceti.mx>
2026-09-24 21:58:08 -0600

    Termine

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://badgyal-astrotuto.netlify.app',
+  integrations: [preact()]
+});
